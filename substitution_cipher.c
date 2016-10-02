328ad6eb7fc15d6d92c51c59d90189470ff6c203
willherr <herrmanw@mail.gvsu.edu>
2016-10-02 12:31:02 -0400

    Add files via upload
    Version 1. Removing duplicates from the key with helper function targetFound

---
+++ substitution_cipher.c
@@ -0,0 +1,109 @@
+/**
+   Substitution Cipher - Project 1
+
+   William Herrmann
+   September 26th, 2016
+ */
+
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define TRUE 1
+#define FALSE 0
+#define NUMALPHA 26
+
+// remove duplicate characters in array word and return the result string
+char * removeDuplicates(char word []);
+
+// search the first num characters in array charArray for character target
+// return a non-zero integer if found, otherwise, return 0
+int targetFound(char charArray[], int num, char target);
+
+// initialize the encrypt array with appropriate cipher letters according to the given key
+void initializeEncryptArray(char key[], char encrypt[]);
+
+// initialize the decrypt array with appropriate substitute letters based on the encrypt array
+void initializeDecryptArray(char encypt[], char decrypt[]);
+
+// process data from the input file and write the result to the output file
+// pass the encrypt array to parameter substitute if encryption is intended
+// pass the decrypt array to parameter substitute if decryption is intended
+void processInput(FILE * inf, FILE * outf, char substitute[]);
+
+int main(int argc, char* argv[])
+{
+  int choice;
+  char *string, *key, encrypt[NUMALPHA+1], decrypt[NUMALPHA+1];
+  FILE *fin, *fout;
+  
+  if (argc != 5){
+    printf ("Usage: cipher option, key (as uppercase string), infile, outfile\n");
+    printf ("Option 1 for encryption and 2 for decryption");
+    exit(1);
+  }
+  
+  choice = atoi(argv[1]);
+  string = argv[2];
+
+  key = removeDuplicates(string);
+  
+  free(key);
+ 
+  return 0;
+}
+
+
+
+//reading and writing encrypting/decrypting operation
+void processInput(FILE * inf, FILE * outf, char substitute[]){
+  
+}
+
+
+
+//decrypt array initialization
+void initializeDecryptArray(char encrypt[], char decrypt[]){
+  
+}
+
+
+
+//encrypt array initialization
+void initializeEncryptArray(char key[], char encrypt[]){
+  
+}
+
+
+
+//helper to search array and find duplicate letter
+int targetFound(char charArray[], int num, char target){
+  int i;
+
+  for(i=0; i < strlen(charArray); i++){
+    if(i < num && charArray[i] == target)
+      return TRUE;
+  }
+
+  return FALSE;  
+}
+
+
+
+//helper to fill a new array the order of the letters from word without duplicates
+char * removeDuplicates(char word []){
+  char *key = (char*) malloc(sizeof(char)*26);
+  int i, k = 0;
+
+  for(i=0; i < strlen(word); i++){
+    if(!targetFound(word, i, word[i])){
+      key[k] = word[i]; //maybe combine next two lines?
+      k++;
+    }
+  }
+  key[k] = '\0';
+
+  printf("\nword: %s\nkey: %s\n", word, key);
+  return key;
+}
